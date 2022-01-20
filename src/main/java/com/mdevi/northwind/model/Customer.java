4f958ec9e68c3e1b14fd925e1e6f115814bb4fa5
Sergei Belonosov <2belonosov@gmail.com>
2022-01-20 11:06:24 +0700

    FIX Validation annotations were fixed within Employee and Customer classes.

--- src/main/java/com/mdevi/northwind/model/Customer.java
+++ src/main/java/com/mdevi/northwind/model/Customer.java
@@ -1,12 +1,13 @@
 package com.mdevi.northwind.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Objects;
 
--- src/main/java/com/mdevi/northwind/model/Employee.java
+++ src/main/java/com/mdevi/northwind/model/Employee.java
@@ -1,9 +1,9 @@
 package com.mdevi.northwind.model;
 
+import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
